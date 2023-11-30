eb32b75c7d3330bb4b12459b15a520d24a434a29
Asraful-code235 <asrafulislamcodes@gmail.com>
2023-11-30 14:15:06 +0600

    Design fixes

--- components/start/Timeline.jsx
+++ components/start/Timeline.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function Timeline() {
   return (
-    <section className=" py-20 px-16 2xl:px-32 bg-white">
+    <section className="visionParagraph py-6 lg:py-20 px-6 lg:px-16 2xl:px-32 bg-white ">
       {/* <h1 className="visionParagraph text-[40px] font-bold leading-[67px] tracking-[0.8px]">
         NeduAi Timeline
       </h1> */}
@@ -90,6 +90,91 @@ export default function Timeline() {
         </div>
       </div>
       {/* mobile */}
+      <div className="relative my-10 h-fit mx-auto flex lg:hidden items-center justify-center gap-3 sm:gap-5">
+        <Image
+          src={"/timeline/mobiletimeline.svg"}
+          width={1000}
+          height={1000}
+          className="w-fit max-[372px]:h-[664px] max-[388px]:h-[70%] h-[698px] max-w-[393px]"
+        />
+        {/* july 2023 */}
+        <div className="flex flex-col max-[387px]:gap-5 gap-7 h-fit -my-2">
+          <div className="px-[7px] py-[11px] bg-[#FF980E] max-w-[258px] bg-opacity-10 border border-[#FF980E] rounded-[10px]  flex flex-col gap-1 items-start justify-start text-start">
+            <h4 className="font-semibold text-[12px] w-full leading-normal text-[#FF980E]">
+              July ‘23
+            </h4>
+            <p className="visionParagraph text-[10px] leading-normal font-semibold">
+              Filip Bovin, Pasi Orovuo, and Kari Savolainen founded NeduAI,
+              <span className="font-normal">
+                {" "}
+                marking the start of a new era in AI-assisted education and
+                recruitment.
+              </span>
+            </p>
+          </div>
+          <div className="px-[7px] py-[11px] bg-[#FF980E] max-w-[258px] bg-opacity-10 border border-[#FF980E] rounded-[10px]  flex flex-col gap-1 items-start justify-start text-start">
+            <h4 className="font-semibold text-[12px] w-full leading-normal text-[#FF980E]">
+              August ‘23
+            </h4>
+            <p className="visionParagraph text-[10px] leading-normal font-semibold">
+              Partnership formed with XAMK University{" "}
+              <span className="font-normal">
+                of Applied Sciences to enhance our service foundation and value
+                proposition. Welcomed new members to our advisory board,{" "}
+              </span>
+              <span className="font-semibold">
+                bringing diverse expertise and insights.
+              </span>
+            </p>
+          </div>
+          <div className="px-[7px] py-[11px] bg-[#FF980E] max-w-[258px] bg-opacity-10 border border-[#FF980E] rounded-[10px]  flex flex-col gap-1 items-start justify-start text-start">
+            <h4 className="font-semibold text-[12px] w-full leading-normal text-[#FF980E]">
+              Q3-4 ‘23
+            </h4>
+            <p className="visionParagraph text-[10px] leading-normal font-semibold">
+              Engaged in extensive networking and sales meetings,{" "}
+              <span className="font-normal leading-normal">
+                exploring potential partnerships.
+              </span>
+            </p>
+          </div>
+          <div className="px-[7px] py-[11px] bg-[#FF980E] max-w-[258px] bg-opacity-10 border border-[#FF980E] rounded-[10px]  flex flex-col gap-1 items-start justify-start text-start">
+            <h4 className="font-semibold text-[12px] w-full leading-normal text-[#FF980E]">
+              October ‘23
+            </h4>
+            <p className="visionParagraph text-[10px] leading-normal font-normal">
+              Engaged in extensive networking and sales meetings,Began
+              development of the{" "}
+              <span className="font-semibold">
+                Alpha version of the NeduAI platform.
+              </span>
+            </p>
+          </div>
+          <div className="px-[7px] py-[11px] bg-[#FF980E] max-w-[258px] bg-opacity-10 border border-[#FF980E] rounded-[10px]  flex flex-col gap-1 items-start justify-start text-start">
+            <h4 className="font-semibold text-[12px] w-full leading-normal text-[#FF980E]">
+              Q4 ‘23
+            </h4>
+            <p className="visionParagraph text-[10px] leading-normal font-semibold">
+              Achieved our first sales and strategic partnership with XAMK,{" "}
+              <span className="font-normal leading-normal">
+                a significant milestone in bringing our vision to life.
+              </span>
+            </p>
+          </div>
+          <div className="px-[7px] py-[11px] bg-[#FF980E] max-w-[258px] bg-opacity-10 border border-[#FF980E] rounded-[10px]  flex flex-col gap-1 items-start justify-start text-start">
+            <h4 className="font-semibold text-[12px] w-full leading-normal text-[#FF980E]">
+              Q1 ‘24
+            </h4>
+            <p className="visionParagraph text-[10px] leading-normal font-semibold">
+              Launching our first investment round,{" "}
+              <span className="font-normal leading-normal">
+                inviting investors to join us in our mission to redefine
+                education and recruitment.
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
